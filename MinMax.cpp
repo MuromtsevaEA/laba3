bce070c774032b87a188bec0b2685a4863b1000e
elvira(Muromtseva) <elvira@example.com>
2020-04-20 21:46:08 +0300

    внесла изменения с учетом случая, при котоом программа не работает

--- MinMax.cpp
+++ MinMax.cpp
@@ -5,6 +5,13 @@ void find_minmax(vector<double> numbers, double& min, double& max)
 {
     min = numbers[0];
     max = numbers[0];
+    if(numbers.size()==0)
+    {
+        min = numbers[0];
+        max = numbers[0];
+    }
+    else
+        {
     for (double number : numbers)
     {
         if (number < min)
@@ -15,5 +22,6 @@ void find_minmax(vector<double> numbers, double& min, double& max)
         {
             max = number;
         }
+    }
     }
 }
