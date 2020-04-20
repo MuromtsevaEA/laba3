7b3f4a46915f4835a4565321d43b146749f82383
elvira(Muromtseva) <elvira@example.com>
2020-04-20 20:54:19 +0300

    добавила MinMax.cpp, переместила в сюда функцию, все сохранила

--- MinMax.h
+++ MinMax.h
@@ -1,21 +1,7 @@
-#ifndef MINMAX_H_INCLUDED
-#define MINMAX_H_INCLUDED
+#ifndef HISTOGRAM_H_INCLUDED
+#define HISTOGRAM_H_INCLUDED
 #include <vector>
-
 using namespace std;
+void find_minmax(const vector<double> numbers, double& min, double& max);
 
-void find_minmax(vector<double> numbers, double& min, double& max)
-{
-    min=numbers[0];
-    max=numbers[0];
-    for(double number:numbers)
-    {
-        if(number>max)
-            max=number;
-        if(number<min)
-            min=number;
-    }
-}
-
-
-#endif // MINMAX_H_INCLUDED
+#endif // HISTOGRAM_H_INCLUDED
