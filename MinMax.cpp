baf424cd735db82cdb9cd283800881b379431f4a
elvira(Muromtseva) <elvira@example.com>
2020-04-27 15:19:30 +0300

    добавила исправления

--- MinMax.cpp
+++ MinMax.cpp
@@ -1,7 +1,7 @@
 #include "MinMax.h"
 #include <iostream>
 
-void find_minmax(vector<double> numbers, double& min, double& max)
+void find_minmax(const vector<double>& numbers, double& min, double& max)
 {
     min = numbers[0];
     max = numbers[0];
