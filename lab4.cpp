e6b095841b8cc3c43076884eabb476f44fb47082
elvira(Muromtseva) <elvira@example.com>
2020-05-15 20:52:10 +0300

    пример из URL

--- lab4.cpp
+++ lab4.cpp
@@ -32,12 +32,19 @@ int main(int argc, char* argv[])
         for(int i=0; i<argc;i++)
         cout<<argv[]<<endl;
         return 0;
-    }
+
+    CURL *curl = curl_easy_init();
+if(curl) {
+  CURLcode res;
+  curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+  res = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+}
     curl_global_init(CURL_GLOBAL_ALL);
    const auto input=read_input(cin, true);
    const auto bins=make_histogram(input);
    show_histogram_svg(bins);
 
     return 0;
-}
+}}
 
